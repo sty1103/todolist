9ea1008c3e3b1f1fd786c242c00da1efbab2714c
Hyeonjun <sty110357@gmail.com>
2022-06-13 14:47:10 +0900

    [INIT]

---
+++ pages/todo/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Todo: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        
+      </Head>
+      <main>
+        Welcome!
+      </main>
+    </div>
+  )
+}
+
+export default Todo;
